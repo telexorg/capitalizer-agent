9c18f7c3c64a46cae8c08e2bb9d958ceb759bc86
Idara-Cecilia Umana <idyumana.iu@gmail.com>
2025-09-06 16:06:05 +0100

    feat: initial commit

---
+++ main.py
@@ -0,0 +1,156 @@
+import os
+from datetime import datetime
+from pprint import pprint
+import uvicorn, json
+from uuid import uuid4
+from fastapi import FastAPI, Request, status, HTTPException, BackgroundTasks
+from fastapi.responses import HTMLResponse
+from dotenv import load_dotenv
+
+from a2a.types import (
+    AgentCard,
+    AgentCapabilities,
+    AgentSkill,
+    AgentProvider
+)
+import a2a.types as a2a_types
+
+
+
+load_dotenv()
+
+TELEX_API_KEY = os.getenv('TELEX_API_KEY')
+TELEX_API_URL = os.getenv('TELEX_API_URL')
+
+app = FastAPI()
+
+
+@app.get("/", response_class=HTMLResponse)
+def read_root():
+    return '<p style="font-size:30px">SEO agent</p>'
+
+
+@app.get("/.well-known/agent.json")
+def get_agent_card(request: Request):
+    external_base = request.headers.get("x-external-base-url", "")
+    current_base_url = str(request.base_url).rstrip("/") + external_base
+
+    capabilities = AgentCapabilities(pushNotifications=True)
+
+    skills = AgentSkill(
+        id= "all-caps",
+        name= "Capitalize all letters",
+        description= "Capitalize all letters provided",
+        inputModes= ["text"],
+        outputModes= ["text"],
+        tags=["capitalize"]
+    )
+
+    provider = AgentProvider(
+       organization="Telex", url="https://telex.im"
+    )
+
+    agent_card = AgentCard(
+        name='Capitalizer',
+        description='Capitalizes stuff',
+        url=current_base_url,
+        version='1.0.1',
+        defaultInputModes=["text/plain"],
+        defaultOutputModes=["text/plain"],
+        capabilities=capabilities,
+        skills=[skills],
+        provider=provider,
+        documentationUrl=f"{current_base_url}/docs"
+    )
+
+    card = AgentCard.model_dump(agent_card)
+
+    return card
+
+
+async def handle_task(message:str, request_id, user_id:str, task_id: str, webhook_url: str, api_key: str, context_id: str):
+    
+    capitalized = message.upper()
+    print(f"after capitalizing {capitalized}")
+
+    parts = a2a_types.TextPart(text=capitalized)
+
+    message = a2a_types.Message(messageId=uuid4().hex, role=a2a_types.Role.agent, parts=[parts])
+
+    artifacts = a2a_types.Artifact(artifactId=uuid4().hex, parts=[parts])
+
+    task = a2a_types.Task(
+        id = task_id,
+        contextId= context_id,
+        status =  a2a_types.TaskStatus(
+        state=a2a_types.TaskState.completed, 
+        message=a2a_types.Message(messageId=uuid4().hex, role=a2a_types.Role.agent, parts=[a2a_types.TextPart(text="Success!")])
+        ),
+        artifacts = [artifacts]
+    )
+
+    print("TASK END: ", datetime.now())
+
+    return task
+
+
+
+@app.post("/")
+async def handle_request(request: Request, background_tasks: BackgroundTasks):
+  try:
+    body = await request.json()
+
+  except json.JSONDecodeError as e:
+    error = a2a_types.JSONParseError(
+      data = str(e)
+    )
+    response = a2a_types.JSONRPCErrorResponse(
+       error=error
+    )
+
+  request_id = body.get("id")
+  user_id = body["params"]["message"]["metadata"].get("telex_user_id", None)  
+  org_id = body["params"]["message"]["metadata"].get("org_id", None)  
+  webhook_url = body["params"]["configuration"]["pushNotificationConfig"]["url"]
+  api_key = body["params"]["configuration"]["pushNotificationConfig"]["authentication"].get("credentials", TELEX_API_KEY)
+
+  message_parts = body["params"]["message"]["parts"]
+
+  if not message_parts:
+    raise HTTPException(
+      status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+      detail="Message cannot be empty."
+    )
+  
+  context_id = uuid4().hex
+  new_task_id  = uuid4().hex
+
+  incoming_message: a2a_types.Part = a2a_types.TextPart.model_validate(message_parts[0])
+
+  print(incoming_message, "message")
+
+  text_message = incoming_message.text
+
+  if not text_message:
+    raise HTTPException(
+      status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+      detail="Message text cannot be empty."
+    )
+  
+  print("TASK START: ", datetime.now())
+  result = await handle_task(text_message, request_id, user_id, new_task_id, webhook_url, api_key, context_id)
+
+
+  response = a2a_types.JSONRPCResponse(
+      id=request_id,
+      result=result
+  )
+
+  response = response.model_dump(exclude_none=True)
+  pprint(response)
+  return response
+
+
+if __name__ == "__main__":
+    port = int(os.getenv("PORT", 5000))
+    uvicorn.run("main:app", host="127.0.0.1", port=port, reload=True)
